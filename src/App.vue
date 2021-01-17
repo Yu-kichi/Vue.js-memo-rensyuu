6369d50fa8219e2bce5f7d2c01f44858b464da7c
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-01-17 22:56:24 +0900

    first commit
    ローカルストレージを親のAppの方で保存

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <todo-form v-on:add="addTodo($event)"></todo-form>
+    <todo-list v-bind:todos="todos" v-on:delete="deleteTodo($event)">
+    </todo-list>
   </div>
 </template>
+
+<script>
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+export default{
+  data(){
+    return{
+      todos:[]
+    }
+  },
+  created () {
+    this.todos = JSON.parse(localStorage.getItem('todos')) || []
+  },
+  components:{
+    'todo-form':TodoForm,
+    'todo-list':TodoList
+  },
+  methods:{
+    addTodo(newTodo) {
+      this.todos.push(newTodo)
+      localStorage.setItem('todos',JSON.stringify(this.todos))
+    },
+    deleteTodo(i){
+      this.todos.splice(i,1)
+      localStorage.setItem('todos',JSON.stringify(this.todos))
+    }
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>My Todo</h1>
+  <input v-model = "newTodo" placeholder = "Input here..">
+  <button v-on:click = "addTodo()">ADD</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+      newTodo:""
+    }
+  },
+  methods:{
+    addTodo () {
+      if(this.newTodo === "") return
+      this.$emit('add',this.newTodo)
+      this.newTodo = ""
+    }
+  }
+}
+</script>
